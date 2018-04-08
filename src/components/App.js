eeb4bca7deefedbae44f6902681ca739bb0c62b7
buonzz <buonzz@gmail.com>
2018-04-08 22:09:41 +0800

    implemented 404

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { Switch, Route, NavLink } from 'react-router-dom';
 import Home from './Home';
 import Page1 from './Page1';
 import Page2 from './Page2';
+import Page404 from './Page404';
 
 class App extends Component{
 	render(){
@@ -20,6 +21,7 @@ class App extends Component{
 		            <Route exact path='/' component={Home}/>
 		            <Route exact path='/page1' component={Page1}/>
 		            <Route exact path='/page2' component={Page2}/>
+		            <Route component={Page404} />
 		        </Switch>
 		        </div>
 			</div>);
---
+++ src/components/Page404.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+const qs = require('query-string');
+
+
+class Page404 extends Component{
+	render(){
+		return(
+			<h3>404 - Page not found</h3>
+		);
+	}
+}
+
+export default Page404;
