768ca14683ab4b880f5b355610d494639598f09c
buonzz <buonzz@gmail.com>
2018-04-08 13:40:24 +0800

    sample parsing of query string parameter

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,11 @@
 import React, {Component} from 'react';
+const qs = require('query-string');
+
 
 class Home extends Component{
 	render(){
+
+		console.log(qs.parse(this.props.location.search));
 		return(
 			<h3>This is home</h3>
 		);
