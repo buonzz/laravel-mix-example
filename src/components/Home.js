26032c231c8878624f42b39e9c1da78d0b2a5866
buonzz <buonzz@gmail.com>
2018-04-12 18:38:20 +0800

    remove material ui dependency

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,11 @@
 import React, {Component} from 'react';
 const qs = require('query-string');
-import RaisedButton from 'material-ui/RaisedButton';
 
 class Home extends Component{
 	render(){
 		return(
 			<div>
 			<h3>This is home</h3>
-			<RaisedButton label="Default" />
 			</div>
 		);
 	}
