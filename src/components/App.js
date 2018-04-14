cd48e61116ecc8ba9ca24babbab832bcb2dd39da
buonzz <buonzz@gmail.com>
2018-04-14 18:49:53 +0800

    react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
-import { Switch, Route, NavLink } from 'react-router-dom';
-
-import { Button } from 'semantic-ui-react';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Grid, Menu, Segment } from 'semantic-ui-react';
 
 import Home from './Home';
 import Page1 from './Page1';
@@ -9,26 +8,44 @@ import Page2 from './Page2';
 import Page404 from './Page404';
 
 class App extends Component{
+
+	constructor(props){
+		super(props);
+		this.state = { activeItem: 'bio' }; 
+		this.handleItemClick = this.handleItemClick.bind(this);
+	}
+
+	handleItemClick(e, { name }){
+		this.setState({ activeItem: name })
+	}
+
 	render(){
 		const params = window.location.search;
+		const { activeItem } = this.state;
+
 		return(
-			<div>
-				<h1>Laravel Mix Example</h1>
-				<ul>
-					<li><NavLink exact to={"/"+params} isActive={(match, location) => location.pathname + location.search === '/'+params}>Home</NavLink></li>
-					<li><NavLink to={"/page1/"+params} isActive={(match, location) => location.pathname + location.search === '/page1/'+params}>Page 1</NavLink></li>
-					<li><NavLink to={"/page2/"+params} isActive={(match, location) => location.pathname + location.search === '/page2/'+params}>Page 2</NavLink></li>
-				</ul>
-				 <Button>Click Here</Button>
-				<div>
-		        <Switch>
-		            <Route exact path='/' component={Home}/>
-		            <Route exact path='/page1/' component={Page1}/>
-		            <Route exact path='/page2/' component={Page2}/>
-		            <Route component={Page404} />
-		        </Switch>
-		        </div>
-			</div>);
+			  <Grid>
+			    <Grid.Column width={4}>
+			      <Menu fluid vertical tabular>
+			        <Menu.Item name='bio' active={activeItem === 'bio'} onClick={this.handleItemClick} as={Link} to='/'/>
+			        <Menu.Item name='pics' active={activeItem === 'pics'} onClick={this.handleItemClick} as={Link} to='/page1/'/>
+			        <Menu.Item name='companies' active={activeItem === 'companies'} onClick={this.handleItemClick} as={Link} to='/page2/'/>
+			        <Menu.Item name='links' active={activeItem === 'links'} onClick={this.handleItemClick} as={Link} to='/page3/'/>
+			      </Menu>
+			    </Grid.Column>
+
+			    <Grid.Column stretched width={12}>
+			      <Segment>
+			        <Switch>
+			            <Route exact path='/' component={Home}/>
+			            <Route exact path='/page1/' component={Page1}/>
+			            <Route exact path='/page2/' component={Page2}/>
+			            <Route component={Page404} />
+			        </Switch>
+			      </Segment>
+			    </Grid.Column>
+			  </Grid>
+		);
 	}
 }
 
