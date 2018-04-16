e8c9900a634778c09ba9da58bb20dfe6c1251231
buonzz <buonzz@gmail.com>
2018-04-16 10:00:36 +0800

    made simple redux work

--- src/components/Page2.js
+++ src/components/Page2.js
@@ -1,11 +1,29 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
 
 class Page2 extends Component{
+
 	render(){
+
+		const libraryList = this.props.libraries.map((library) => 
+			<li key={library.id}>
+				{library.title}
+			</li>);
+
 		return(
-			<h3>This is page 2</h3>
+			<div>
+				<h3>Libraries</h3>
+				<ul>
+					{libraryList}
+				</ul>
+			</div>
 		);
 	}
 }
 
-export default Page2;
+
+const mapStateToProps = state => {
+  return { libraries: state.libraries };
+};
+
+export default connect(mapStateToProps)(Page2);
